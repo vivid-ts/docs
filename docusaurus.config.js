d211f50b060bfbab5fcc7e7fa910da7869df5fe1
Edqe_ <34704796+Edqe14@users.noreply.github.com>
2023-05-28 16:43:05 +0700

    refactor: home

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Vivid',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Free & Open-source Admin Template',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,36 +5,23 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+      title="Home"
+      description="Homepage for Vivid.ts"
+    >
+      <main className='container'>
+        <section className='flex flex-col items-center pt-16'>
+          <img src="/img/logo.svg" alt="Logo" className='w-24 mb-8' />
+
+          <h1 className='font-semibold text-4xl'>{siteConfig.title}</h1>
+          <p>{siteConfig.tagline}</p>
+        </section>
+
+        {/* <HomepageFeatures /> */}
       </main>
     </Layout>
   );
